92a1e956e89980ce10652bd6ee8a4480c64e9d9b
Lucas <lucash.h.h74@gmail.com>
2022-07-07 10:12:58 -0300

    serviço que cria pedidos

--- src/modules/orders/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/typeorm/repositories/OrdersRepository.ts
@@ -14,7 +14,7 @@ interface IRequest {
 }
 
 @EntityRepository(Order)
-export class ProductRepository extends Repository<Order> {
+export class OrdersRepository extends Repository<Order> {
   public async findById(id: string): Promise<Order | undefined> {
     const order = this.findOne(id, {
       relations: ['order_products', 'customer'],
